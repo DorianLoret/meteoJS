03652710897163ece9cc8475f4f862daa90907c9
Dorian <dorian.loret20@gmail.com>
2023-02-16 16:44:20 +0100

    temperatures done

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,10 @@
 let sunrise;
+let meteoData = [];
 let saintsData = [];
 const psunrise = document.querySelector("body > main > section.ephemeride > p:nth-child(4) > span");
+const ptempmax = document.querySelector("body > main > section.temp > div > figure:nth-child(2) > p");
+const ptempmin = document.querySelector("body > main > section.temp > div > figure:nth-child(1) > p");
+const ptempav = document.querySelector("body > main > section.temp > div > figure:nth-child(3) > p");
 
 async function fetchsunrise() {
     await fetch("https://api.meteo-concept.com/api/ephemeride/0?token=6420e2b831dad423bc7ba0412ce1a74f8f094153f4b2ea617612d0c72b7e8ead&insee=31555").then((res) => res.json())
@@ -10,12 +14,27 @@ async function fetchsunrise() {
 
 }
 
-async function fetchsaintsData() {
+async function fetchMeteoData() {
+    await fetch("https://api.meteo-concept.com/api/forecast/daily?token=6420e2b831dad423bc7ba0412ce1a74f8f094153f4b2ea617612d0c72b7e8ead&insee=31555").then((res) => res.json())
+        .then((data) => meteoData = data.forecast)
+
+    ptempmax.innerHTML = meteoData[0].tmax;
+    ptempmin.innerHTML = meteoData[0].tmin;
+    ptempav.innerHTML = Math.round((meteoData[0].tmax + meteoData[0].tmin) / 2);
+
+    console.log(meteoData);
+
+
+
+}
+
+
+
+async function fetchSaintsData() {
     await fetch("https://nominis.cef.fr/json/nominis.php")
         .then((res) => res.json())
         .then((data) => saintsData = data)
 }
 
 
-fetchsunrise();
-
+fetchMeteoData();
