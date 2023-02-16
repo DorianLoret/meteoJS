50eacfbe4c9d46e39c16a903e10553336fcf06a2
Dorian <dorian.loret20@gmail.com>
2023-02-16 16:16:31 +0100

    sunrise done

--- src/js/index.js
+++ src/js/index.js
@@ -1,15 +1,21 @@
-let sunData = [];
+let sunrise;
 let saintsData = [];
+const psunrise = document.querySelector("body > main > section.ephemeride > p:nth-child(4) > span");
 
-async function fetchsunData() {
- await fetch("https://api.meteo-concept.com/api/ephemeride/0?token=6420e2b831dad423bc7ba0412ce1a74f8f094153f4b2ea617612d0c72b7e8ead&insee=31555").then((res) => res.json())
-    .then((data) => sunData = data)
+async function fetchsunrise() {
+    await fetch("https://api.meteo-concept.com/api/ephemeride/0?token=6420e2b831dad423bc7ba0412ce1a74f8f094153f4b2ea617612d0c72b7e8ead&insee=31555").then((res) => res.json())
+        .then((data) => sunrise = data.ephemeride.sunrise)
+
+    psunrise.innerHTML = sunrise;
+
 }
 
 async function fetchsaintsData() {
- await fetch("https://nominis.cef.fr/json/nominis.php")
-    .then((res) => res.json())
-    .then((data) => console.log(data))
+    await fetch("https://nominis.cef.fr/json/nominis.php")
+        .then((res) => res.json())
+        .then((data) => saintsData = data)
 }
 
-fetchsaintsData();
+
+fetchsunrise();
+
